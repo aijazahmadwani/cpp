a74ba38fe3a129144efdd6d33598ce6c820c5c72
aijazahmadwani <aijazahmad9864@gmail.com>
2019-04-01 18:44:13 +0530

    square of number

---
+++ class/class.cpp
@@ -0,0 +1,5 @@
+/*Author: AIJAZ AHMAD WANI
+email: aijazahmad9864@gmail.com
+Date: 1/4/2019
+*/
+
---
+++ square_of_a_number/square_of_no.cpp
@@ -0,0 +1,17 @@
+/*Author: AIJAZ AHMAD WANI
+email: aijazahmad9864@gmail.com
+Date: 1/4/2019
+*/
+//program to find square of a number
+#include<iostream>
+int main()
+{
+    int n,result;
+    std::cout<<"Enter a number : ";
+    std::cin>>n;
+    result=n*n;
+    std::cout<<"Square of "<<n<<" is "<<result<<std:: endl;
+
+
+    return 1;
+}
