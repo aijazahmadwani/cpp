3cfa5d83ff969fe79983fce55e177cd99e3b78da
aijazahmadwani <aijazahmad9864@gmail.com>
2019-04-04 18:32:54 +0530

    manipulators

---
+++ manipulators/manipulators.cpp
@@ -0,0 +1,21 @@
+/*
+Author : Aiajz Ahmad Wani
+email : aijazahmad9864@gmail.com
+Date : 4 /4 2019*/
+// program to use various manipulators in c++
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    //manipulator operator
+    //endl : newline operator
+    cout << "AIJAZ" << endl << "AHMAD" << endl<< "WANI"<< endl;  
+    //setw() used to set width
+    //manipulator functions
+    cout << 1;  
+    return 0;
+    
+}
