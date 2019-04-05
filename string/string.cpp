b661efa05fd5f80d3b63e50a5e90e3a314bc43c6
aijazahmadwani <aijazahmad9864@gmail.com>
2019-04-05 18:26:51 +0530

    string

---
+++ string/string.cpp
@@ -0,0 +1,14 @@
+/*Author: AIJAZ AHMAD WANI
+email: aijazahmad9864@gmail.com
+Date: 5/4/2019
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string ptr; 
+	ptr = "hello world";
+	cout << ptr;
+	return 0;
+}
