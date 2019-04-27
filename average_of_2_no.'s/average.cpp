41ac23ddbb43071651c845358e46032b695f5b59
aijazahmadwani <aijazahmad9864@gmail.com>
2019-04-27 22:26:40 +0530

    average of two numbers

---
+++ average_of_2_no.'s/average.cpp
@@ -0,0 +1,18 @@
+/*Author: AIJAZ AHMAD WANI
+email: aijazahmad9864@gmail.com
+Date: 27/ 4/ 2019 */
+// average of two numbers
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float num1,num2,sum=0,average;
+	cout<<"Enter two numbers : ";
+	cin >> num1 >> num2;
+	sum = num1 + num2;
+	average = sum / 2;
+	cout <<"Average = " <<average;
+	return 0;
+
+} 
