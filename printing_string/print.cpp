e4b44c03dd3b8e97c52ac34f646929491c65ce9b
aijazahmadwani <aijazahmad9864@gmail.com>
2019-04-27 22:20:22 +0530

    printing string

---
+++ printing_string/print.cpp
@@ -0,0 +1,13 @@
+/*Author: AIJAZ AHMAD WANI
+email: aijazahmad9864@gmail.com
+Date: 27/ 4/ 2019
+*/
+//printing a string
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello world!"<<endl;
+	return 0;
+}
