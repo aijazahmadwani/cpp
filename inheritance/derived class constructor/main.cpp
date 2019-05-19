69f30d03c5dc4a59ecf68c682ce1040c116a0a06
aijazahmadwani <aijazahmad9864@gmail.com>
2019-05-19 10:17:39 +0530

    derived class constructor

--- inheritance/derived class constructor/main.cpp
+++ inheritance/derived class constructor/main.cpp
@@ -3,3 +3,65 @@ email: aijazahmad9864@gmail.com
 Date: 19/ 5/ 2019
 */
 // constructors in derived class
+
+#include <iostream>
+using namespace std;
+class counter		//base class
+{
+protected:
+	unsigned int count;
+public:
+	counter() : count()	//no argument constructor
+	{
+
+	}
+	counter(int c) : count(c)	// 1 argument constructor
+	{
+
+	}
+	unsigned int get_count() const 	//return count
+	{
+		return count;
+	}
+	counter operator ++ ()		//increment count (prefix)
+	{
+		return counter(++count);
+	}
+};
+
+class countDn : public counter	// derived class
+{
+public:
+	countDn() : counter()		//constructor, no arguments
+	{
+
+	}
+	countDn(int  c): counter (c)	// constructor , 1 arg
+	{
+
+	}
+	counter operator --()		// decrement count (prefix)
+	{
+		return counter(--count);
+	}
+};
+
+int main()
+{
+	countDn c1;
+	countDn c2(100);
+
+	cout <<"\nc1 = "<<c1.get_count();
+	cout <<"\nc2 = "<<c2.get_count();
+
+	++c1;	++c1;	++c1;
+	cout <<"\nc1 = "<<c1.get_count();
+
+	--c2;	--c2;
+	cout <<"\nc2 = "<<c2.get_count();
+
+	countDn c3;
+	cout <<"\nc3 = "<<c3.get_count();
+	cout <<endl;
+	return 0;
+}
