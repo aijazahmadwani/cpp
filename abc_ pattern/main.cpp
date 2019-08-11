fc89cdced20d6cda0f55367a025bc3432a61b6bb
aijazahmadwani <aijazahmad9864@gmail.com>
2019-08-11 11:28:44 +0530

    abs pattern

---
+++ abc_ pattern/main.cpp
@@ -0,0 +1,22 @@
+
+/*
+Author: Aijaz Ahmad Wani
+email : aijazahmand9864@gmail.com
+Date : 11-08-2019
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int l=65;l<=91;l++)
+    {
+       for(int i=65;i<l;i++){
+        char ch=i;
+        cout<<ch<<" ";
+       }
+        cout<<endl;
+    }
+
+}
